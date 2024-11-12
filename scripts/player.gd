7de6f7e335a906d9abfa035cf1e06238f257fe70
Rally <59124512+TRGRally@users.noreply.github.com>
2024-11-12 14:09:51 +0000

    input mapping

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const AIR_FRICTION = 0.95
+const FRICTION = 0.9
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +13,32 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+		
+	
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontalDirection := Input.get_axis("move_left", "move_right")
+	var verticalDirection := Input.get_axis("move_up", "move_down")
+	
+	var inputVector = Vector2.ZERO
+	inputVector.x = horizontalDirection
+	inputVector.y = verticalDirection
+	
+	if horizontalDirection:
+		velocity.x = inputVector.x * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = velocity.x * FRICTION
+	
+	if not is_on_floor():
+		velocity.x = velocity.x * AIR_FRICTION
+			
+			
+		
+	
+		
+	
 	move_and_slide()
