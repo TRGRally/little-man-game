76937d7eed23fc3086332182d096161fb9b29f85
TRGRally <59124512+TRGRally@users.noreply.github.com>
2024-11-14 01:39:44 +0000

    dash period immune to gravity and friction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
+const SPEED = 50.0
+const AIR_SPEED = 20.0
 const JUMP_VELOCITY = -600.0
 const AIR_FRICTION = 0.95
-const FRICTION = 0.9
+const FRICTION = 0.8
 const GRAVITY = 2000
 const FALL_GRAVITY = 2400
-const DASH_VELOCITY = 1000
+const DASH_VELOCITY = 600
+const DASH_GRAVITY = 0
+const DASH_FRICTION = 0
+const DASH_TIME_S = 0.2
 
+var isDashing = false
 var dashCount = 0
 var allowedDashes = 1
 
@@ -31,6 +36,8 @@ func _ready():
 
 
 func is_dash_available():
+	if isDashing:
+		return false
 	if dashCount >= allowedDashes:
 		return false
 	return true
@@ -41,15 +48,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		#fucked up nested if cause move_down should be the switch
 		if is_on_floor():
+			$Sprite2D.animation = "duck"
 			hitbox_shape.size = shrunk_hitbox_size
 			hitbox.position = shrunk_hitbox_transform
 	else:
+		$Sprite2D.animation = "default"
 		hitbox_shape.size = normal_hitbox_size
 		hitbox.position = normal_hitbox_transform
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		if not isDashing:
+			velocity.y += GRAVITY * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -63,28 +73,36 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
+		dashCount = 0
 		
 	
 	if horizontalDirection:
-		velocity.x += inputVector.x * SPEED
+		if is_on_floor():
+			velocity.x += inputVector.x * SPEED
+		else:
+			velocity.x += inputVector.x * AIR_SPEED
 
 		
 	
 	if not is_on_floor():
 		velocity.x = velocity.x * AIR_FRICTION
 	else:
-		velocity.x = velocity.x * FRICTION
+		if not isDashing:
+			velocity.x = velocity.x * FRICTION
 		
 	#Dash
 	if Input.is_action_just_pressed("dash") and is_dash_available():
+		isDashing = true
 		dashCount = dashCount + 1
+		
+		$DashTimer.start()
 		
 		if velocity.length() <= DASH_VELOCITY:
 			velocity = DASH_VELOCITY * inputVector
 		else:
 			velocity = velocity.length() * inputVector
 	
-	if is_on_floor():
+	if is_on_floor() and not isDashing:
 		dashCount = 0
 			
 			
@@ -93,3 +111,7 @@ func _physics_process(delta: float) -> void:
 		
 	
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	isDashing = false
