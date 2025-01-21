2d6c30cb5c40aad48f445e55132a0c5204b029e1
TRGRally <59124512+TRGRally@users.noreply.github.com>
2025-01-21 20:54:24 +0000

    capture and parse inputs

--- global.gd
+++ global.gd
@@ -1,5 +1,15 @@
 extends Node
 
+var inputAlphabet: Array[StringName] = InputMap.get_actions().filter(func(action): return not action.begins_with("ui_"))
+var tapeAlphabet: Array[StringName]
+
+var frameCounter: int = 0
+var inputsThisFrame: Array[StringName] = []
+
+#Array of tuple(string, int) but that doesnt exist in gdscript so its an array of arrays
+var inputHistory: Array[Array]
+
+
 var saveData: Dictionary = {}
 
 var allLevels: Dictionary
@@ -63,9 +73,16 @@ func get_all_files(path: String, file_ext := "", files : Array[String] = []) -> 
 
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#construct + and - inputs from the input alphabet
+	for action in inputAlphabet:
+		var pressedAction = "+" + action
+		var releasedAction = "-" + action
+		tapeAlphabet.append(StringName(pressedAction))
+		tapeAlphabet.append(StringName(releasedAction))
+	
+	
 	#loads the savedata into globally accessible saveData dictionary
 	loadFromFile()
 	
@@ -83,6 +100,20 @@ func _ready() -> void:
 	
 	
 func _input(event) -> void:
+	
+	if event is InputEventMouseMotion:
+		return
+	
+	for action in inputAlphabet:
+		if event.is_action_pressed(action):
+			var tapeAction = StringName("+" + action)
+			inputsThisFrame.append(tapeAction)
+			
+		if event.is_action_released(action):
+			var tapeAction = StringName("-" + action)
+			inputsThisFrame.append(tapeAction)
+			
+	
 	if event.is_action_pressed("menu"):
 		print("menu pressed")
 		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
@@ -92,3 +123,19 @@ func _input(event) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+#we track physics process in here to know what game frame we're on. for input recording.
+func _physics_process(delta: float) -> void:
+	frameCounter = Engine.get_physics_frames()
+	if inputsThisFrame.size() == 0:
+		return
+	
+	var symbol = [inputsThisFrame, frameCounter]
+	inputHistory.append(symbol)
+	inputsThisFrame = []
+	print(symbol)
+		
+		
+		
+	
+	
